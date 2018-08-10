de862e596c295c386f9fd2ad08d8b96549eb49d7
Christopher Wong <christopherpk1996@gmail.com>
2018-08-11 00:11:56 +0800

    listeners

--- client/screens/canvas-page/canvas-page.jsx
+++ client/screens/canvas-page/canvas-page.jsx
@@ -20,20 +20,54 @@ class Component extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			nodes: [],
-			links: []
+			model: new DiagramModel()
 		};
-		// setup default engine
 		this.engine = new DiagramEngine();
 		this.engine.registerNodeFactory(new DefaultNodeFactory());
 		this.engine.registerLinkFactory(new DefaultLinkFactory());
+		this.engine.setDiagramModel(this.state.model);
 	}
 
 	render() {
-		// regenerate model
-		const model = new DiagramModel();
+		this.engine.setDiagramModel(this.configModel(this.createModel(this.props.nodes, this.props.links)));
+		return (
+			<div
+				style={styles.canvas}
+			>
+				<DiagramWidget
+					allowLooseLinks={false}
+					maxNumberPointsPerLink={0}
+					diagramEngine={this.engine}
+				/>
+			</div>
+		);
+	}
+
+	componentDidMount() {
+		const node1 = new DefaultNodeModel('Function Node 1');
+		node1.addPort(new DefaultPortModel(true, 'out-1', 'IN'));
+		node1.addPort(new DefaultPortModel(false, 'out-2', 'Out'));
+		this.props.store.dispatch(ReactDiagramsAction.addNode(node1));
+		const node2 = new DefaultNodeModel('Function Node 2');
+		node2.addPort(new DefaultPortModel(true, 'out-1', 'IN'));
+		node2.addPort(new DefaultPortModel(false, 'out-2', 'Out'));
+		this.props.store.dispatch(ReactDiagramsAction.addNode(node2));
+	}
+
+	createModel = (nodes, links) => {
+		let model = new DiagramModel();
+		nodes.forEach((item) => {
+			model.addNode(item);
+		});
+		links.forEach((item) => {
+			model.addLink(item);
+		});
+		return model;
+	};
+
+	configModel = (model) => {
 		model.setGridSize(0);
-		// listeners
+		model.clearListeners();
 		model.addListener({
 			linksUpdated: ({ link }) => {
 				link.addListener({
@@ -55,38 +89,8 @@ class Component extends React.Component {
 				});
 			}
 		});
-		// retrieve nodes from redux
-		this.props.nodes.forEach((item) => {
-			model.addNode(item);
-		});
-		this.props.links.forEach((item) => {
-			model.addLink(item);
-		});
-		this.engine.setDiagramModel(model);
-		return (
-			<div
-				style={styles.canvas}
-			>
-				<DiagramWidget
-					allowLooseLinks={false}
-					maxNumberPointsPerLink={0}
-					diagramEngine={this.engine}
-				/>
-			</div>
-		);
-	}
-
-	componentDidMount() {
-		// demo code
-		const node1 = new DefaultNodeModel('Function Node 1');
-		node1.addPort(new DefaultPortModel(true, 'out-1', 'IN'));
-		node1.addPort(new DefaultPortModel(false, 'out-2', 'Out'));
-		this.props.store.dispatch(ReactDiagramsAction.addNode(node1));
-		const node2 = new DefaultNodeModel('Function Node 2');
-		node2.addPort(new DefaultPortModel(true, 'out-1', 'IN'));
-		node2.addPort(new DefaultPortModel(false, 'out-2', 'Out'));
-		this.props.store.dispatch(ReactDiagramsAction.addNode(node2));
-	}
+		return model;
+	};
 
 }
 
