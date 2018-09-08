73d3f8ad5cafb4540728625572b75e16a2249270
Christopher Wong <christopherpk1996@gmail.com>
2018-09-08 22:16:44 +0800

    array map node

---
+++ shared/lib/array-map-node.js
@@ -0,0 +1,42 @@
+import { BasicNode } from "./basic-node";
+
+export class ArrayMapNode extends BasicNode {
+
+	constructor() {
+		super({
+			meta: {
+				label: 'Array Map'
+			},
+			props: {},
+			input: {
+				array: {
+					meta: {
+						label: 'array'
+					}
+				},
+				function: {
+					meta: {
+						label: 'function'
+					}
+				}
+			},
+			output: {
+				array: {
+					meta: {
+						label: 'array'
+					}
+				}
+			},
+			execute: (props, input) => {
+				return new Promise(async (resolve, reject) => {
+					let result = [];
+					for (let i = 0; i !== input['array'].length; i++) {
+						result.push(await input['function'].execute(input['array'][i]));
+					}
+					resolve(result);
+				});
+			}
+		});
+	}
+
+}
