1f4dd88ed3c13929f245b26b05552d5f4e79aff4
httpak <httpak@connect.ust.hk>
2019-03-03 15:42:00 +0800

    typo

--- client/screens/create-project-page/router.js
+++ client/screens/create-project-page/router.js
@@ -4,7 +4,7 @@ import { CreateProjectPage } from "./create-project-page";
 
 FlowRouter.route('/create', {
 	action: (params) => {
-		if(!Meteor.userId() && FlowRouter.current().path === '/canvas'){
+		if(!Meteor.userId() && FlowRouter.current().path === '/create'){
 			FlowRouter.go('/login');
 		}
 		document.title = 'meteor-react-starter';
