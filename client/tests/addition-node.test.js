958782faaa65413cd9aac3287c7b7916c1b49da9
Christopher Wong <christopherpk1996@gmail.com>
2019-03-08 00:08:49 +0800

    dev-177: Bugfix AdditionNode

--- client/tests/addition-node.test.js
+++ client/tests/addition-node.test.js
@@ -2,7 +2,7 @@ import { AdditionNode } from "../../shared/lib/arithmetics/addition-node";
 
 describe('AdditionNode', function () {
 	it('should return sum', async function () {
-		expect(await NumberNode.executor({
+		expect(await AdditionNode.executor({
 			addend: 23,
 			augend: 32
 		}, {})).toEqual({
@@ -10,7 +10,7 @@ describe('AdditionNode', function () {
 		});
 	});
 	it('should return sum of positive and negative number', async function () {
-		expect(await NumberNode.executor({
+		expect(await AdditionNode.executor({
 			addend: 23,
 			augend: -23
 		}, {})).toEqual({
@@ -18,7 +18,7 @@ describe('AdditionNode', function () {
 		});
 	});
 	it('should return sum of zero', async function () {
-		expect(await NumberNode.executor({
+		expect(await AdditionNode.executor({
 			addend: 0,
 			augend: 0
 		}, {})).toEqual({
