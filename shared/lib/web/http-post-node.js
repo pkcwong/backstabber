dd2beff5c43563e84d93a4e43af099a26573f5f5
Christopher Wong <christopherpk1996@gmail.com>
2019-03-31 00:29:27 +0800

    dev-253: Implemented HttpPostNode

---
+++ shared/lib/web/http-post-node.js
@@ -0,0 +1,63 @@
+import { BasicNode } from "../basic-node";
+
+export class HttpPostNode extends BasicNode {
+
+	static props = {};
+
+	static ports = {
+		inputs: {
+			url: (x) => {
+				if (!((x) => {
+					return (/^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/).test(x);
+				})(x)) {
+					throw 'Invalid url';
+				}
+				return x;
+			},
+			header: (x) => {
+				if (typeof x !== 'object') {
+					throw 'Invalid header';
+				}
+				return (x !== null ? x : {});
+			},
+			body: (x) => {
+				return x;
+			}
+		},
+		outputs: {
+			status: (x) => {
+				return (typeof x === 'number');
+			},
+			response: (x) => {
+				return true;
+			}
+		}
+	};
+
+	static executor = (props = HttpPostNode.props, inputs) => {
+		return new Promise((resolve, reject) => {
+			Meteor.call('Request/POST', {
+				url: inputs.url,
+				header: inputs.header,
+				body: inputs.body
+			}, (err, res) => {
+				if (err) {
+					reject(err);
+					return;
+				}
+				resolve({
+					status: res.statusCode,
+					response: res.data ? res.data : res.content
+				});
+			});
+		});
+	};
+
+	constructor(props) {
+		super({
+			class: HttpPostNode,
+			props: props
+		});
+	}
+
+}
